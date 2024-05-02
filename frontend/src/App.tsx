65f7da011c506361729f97cbbbe1629e1e776d4d
ibro001 <ibrahimmoses56@gmail.com>
2024-05-02 19:19:49 +0100

    added my hotel view and added e2e test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 const App = () => {
 
@@ -17,7 +18,8 @@ const App = () => {
         <Route path="/sign-in" element={<Layouts><SignIn /></Layouts>} />
 
         {isLoggedIn && (<>
-          <Route path="/add-hotel" element={<Layouts><AddHotel></AddHotel></Layouts>} />
+          <Route path="/add-hotel" element={<Layouts><AddHotel /></Layouts>} />
+          <Route path="/my-hotels" element={<Layouts><MyHotels /></Layouts>} />
         </>)}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
